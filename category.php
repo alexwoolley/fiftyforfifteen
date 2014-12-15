6ceebeb2af85b2dd571628fd8f233322770d9519
Alexander Woolley <amrwoolley@gmail.com>
2014-12-15 13:14:53 +0000

    Layout fixes, sidebar-cat introduced

--- category.php
+++ category.php
@@ -7,8 +7,8 @@
 	$constituency_id = get_cat_ID( $constituency );	
 ?>
 
-<div id="container" class="clear">
-<div id="content">
+<!--<div id="container" class="clear">-->
+<!--<div id="content">-->
 
 <div class="content-title">
 <?php printf('%s', $constituency); ?>
@@ -16,60 +16,56 @@
 
 <div id="loop" class="list clear">
 
-<?php	
-	//Get constituency profile
-	$query = new WP_Query(array( 'category__and' => array( $constituency_id, 200 ) ));
-	if ($query->have_posts()) :
-		while ( $query->have_posts() ) : $query->the_post(); ?>
-			<div class="post-content post">
-			<?php the_content(); ?>
-			</div>
-		<?php endwhile;
-		wp_reset_query();
-	endif;
-?>
+	<?php	
+		//Get constituency profile
+		$query = new WP_Query(array( 'category__and' => array( $constituency_id, 200 ) ));
+		if ($query->have_posts()) :
+			while ( $query->have_posts() ) : $query->the_post(); ?>
+				<div class="post-content post">
+					<?php the_content(); ?>
+				</div>
+			<?php endwhile;
+			wp_reset_query();
+		endif;
+	?>
 
-<?php
-	$query = new WP_Query("cat=$constituency_id,-200");
-?>
+	<?php
+		$query = new WP_Query("cat=$constituency_id,-200");
 
-<?php if ( $query->have_posts() ) : ?>
+	if ( $query->have_posts() ) : ?>
 
+	<div class="content-title">
+		Our articles
+	</div>
 
+	<?php
+			// Start the Loop.
+			while ( $query->have_posts() ) : $query->the_post(); ?>
+			<div class="post">
+				<div class="thumb">
+					<?php the_post_thumbnail(); ?>
+				</div>
+				<h2>
+					<?php the_title();?>
+				</h2>
+				<div class="post-content">
+					<?php the_excerpt(); ?>
+				</div><!-- .post-content-->
+			</div><!--.post-->
+			<?php
+			endwhile;
 
-<div class="content-title">Our articles</div>
+			wp_reset_query();
 
-<?php
-
-		// Start the Loop.
-		while ( $query->have_posts() ) : $query->the_post(); ?>
-		<div class="post">
-		<div class="thumb">
-		<?php
-		the_post_thumbnail();
-		?>
-		</div>
-		<div class="content-title"><?php the_title();?></div>
-		<div class="post-content">
-		<?php
-		the_excerpt();
-		?>
-		</div>
-
-		</div><!--.post-->
-		<?php
-		endwhile;
-
-		wp_reset_query();
-
-	endif;
-?>
-</div>
-</div><!-- #content -->
-</div><!-- #container -->
+		endif;
+	?>
+</div><!-- #loop -->
+<!--</div> #content -->
+<!--</div> #container -->
 
 
 <?php
-//get_sidebar( 'content' );
+//include ('sidebar-cat.php');
+get_sidebar( 'cat' );
 //get_sidebar();
 get_footer();
--- functions.php
+++ functions.php
@@ -10,3 +10,23 @@ function favicon_link() {
     echo '<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />';
 }
 add_action( 'wp_head', 'favicon_link' );
+
+//For category sidebar
+// if ( function_exists('register_sidebar') ) {
+// 	register_sidebar(array(
+// 		'name' => 'Category sidebar',
+// 		'id' => 'sidebar-cat',
+// 		'description' => 'Appears as the sidebar on category, i.e. constituency, pages',
+// 		'before_widget' => '<div id="%1$s" class="%2$s widget">',
+// 		'after_widget' => '</div></div>',
+// 		'before_title' => '<h3>',
+// 		'after_title' => '</h3><div class="widget-body clear">'
+// 	));
+// }
+
+// if (is_category()) {
+// 	get_sidebar('sidebar-cat');
+// } 
+// else {
+// get_sidebar();
+// }
