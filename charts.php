4c5e2357a0ed82129058eb6c41de485c28b65cc2
Alexander Woolley <amrwoolley@gmail.com>
2015-03-12 13:59:22 +0000

    Arrays in place for 1979

--- charts.php
+++ charts.php
@@ -1,6 +1,6 @@
 <?php 
 //Get the data for the charts
-include(locate_template('charts_data.php'));
+  include(locate_template('charts_data.php'));
 ?>
 
 <!--Load the AJAX API-->
@@ -16,30 +16,61 @@ include(locate_template('charts_data.php'));
   // Callback that creates and populates a data table,
   // instantiates the chart, passes in the data and
   // draws it.
-  function drawChart() {
+  // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
 
-    // Create the data table.
-   var data = google.visualization.arrayToDataTable([
-      <?php echo $data; ?>
-    ]);
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Year');
+        data.addColumn('number', 'Labour');
+        data.addColumn({'type': 'string', 'role': 'tooltip', 'p': {'html': true}});
+        data.addColumn('number', 'Conservative');
+        data.addColumn({'type': 'string', 'role': 'tooltip', 'p': {'html': true}});
+        data.addColumn('number', 'Lib Dems');
+        data.addColumn({'type': 'string', 'role': 'tooltip', 'p': {'html': true}});
+        data.addColumn('number', 'UKIP');
+        data.addColumn({'type': 'string', 'role': 'tooltip', 'p': {'html': true}});
+        data.addColumn('number', 'Green');
+        data.addColumn({'type': 'string', 'role': 'tooltip', 'p': {'html': true}});
+        data.addColumn('number', 'SNP');
+        data.addColumn({'type': 'string', 'role': 'tooltip', 'p': {'html': true}});
+        data.addColumn('number', 'Plaid Cymru');
+        data.addColumn({type:'string', role:'tooltip'});
+        data.addRows([
+            <?php echo $data; ?>
+        ]);
 
-    // Set chart options
-    var options = {
-                    width:600,
-                    height:425,
-                    legend: {position: 'none'}
-                  };
+        // Set chart options
+        var options = {
+                        title: 'Edinburgh South general election results',
+                        hAxis: {title: 'Year',  titleTextStyle: {color: '#333'}},
+                        vAxis: {minValue: 0},
+                        colors: ['#E90000', '#0089DB', '#FFDF00', '#7E0088', '#5AC72C', '#EDE600', '#009131'],
+                        tooltip: {isHtml: true}
+                      };
 
-    // Create a formatter.
-    var formatter = new google.visualization.NumberFormat({pattern:'#,###'});
+        // Create a formatter.
+        //var formatter = new google.visualization.NumberFormat({pattern:'#,###'});
 
-    // Reformat our data.
-    formatter.format(data, 1);
+        // Reformat our data.
+        // formatter.format(data, 1);
+        // formatter.format(data, 2);
 
-    // Instantiate and draw our chart, passing in some options.
-    var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
-    chart.draw(data, options);
-    // var chart_2005 = new google.visualization.ColumnChart(document.getElementById('chart_div_2005'));
-    // chart_2005.draw(data_2005, options_2005);
-  }
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+
+      }
+
+    function createTooltip(candidate, party, votes) {
+        //See http://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
+        var betterVotes = votes.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        return '<div style="padding:5px 5px 5px 5px;">' +
+            candidate + ' (' + party + ')' + '<br>' + 
+            '<strong>' + betterVotes + '</strong>' + ' votes' + '<br>' +
+            // '<strong>' + share + '%' + '</strong>' + ' of the turnout' +
+            '</div>';
+    }
 </script>
--- charts_data.php
+++ charts_data.php
@@ -1,20 +1,115 @@
 <?php
-if (is_category("Edinburgh South")) {
-  $data = "['Party', 'Votes', { role: 'style' }],
-     ['Labour', 15251, '#E90000'],
-     ['Lib Dem', 14899, '#FFDF00'],
-     ['Conservative', 9452, '#0089DB'],
-     ['SNP', 3354, '#EDE600'],
-     ['Green', 881, '#5AC72C']";
-}
-elseif (is_category("Dundee East")) {
-    // NB these are actually the data for Edinburgh South in 2005
-  $data = "['Party', 'Votes', { role: 'style' }],
-     ['Labour', 13251, '#E90000'],
-     ['Lib Dem', 14899, '#FFDF00'],
-     ['Conservative', 9452, '#0089DB'],
-     ['SNP', 3354, '#EDE600'],
-     ['Green', 881, '#5AC72C']";
-}
+
+    $data_1979 = array(
+        'year' => '1979',
+        'labour' => array(
+            'votes' => 15526, 
+            'candidate' => 'some person or other'
+        ),
+        'conservative' => array(
+            'votes' => 17986, 
+            'candidate' => 'some person or other'
+        ),
+        'libdem' => array(
+            'votes' => 7400, 
+            'candidate' => 'some person or other'
+        ),
+        'ukip' => array(
+            'votes' => null, 
+            'candidate' => 'some person or other'
+        ),
+        'green' => array(
+            'votes' => 552, 
+            'candidate' => 'some person or other'
+        ),
+        'snp' => array(
+            'votes' => 3800, 
+            'candidate' => 'some person or other'
+        ),
+        'plaid' => array(
+            'votes' => null, 
+            'candidate' => 'some person or other'
+        )
+    );
+
+    $data_1979_string = "[";
+
+    foreach ($data_1979 as $key => $value) {
+        if ($key == 'year') {
+            $data_1979_string .= "'" . $value . "'" . ",";
+        }
+        else {
+            if ( isset( $value['votes'] ) ) {
+                $data_1979_string .= $value['votes'] . ",";
+                $data_1979_string .= "createTooltip(" . 
+                    "'" . $value['candidate'] . "'," . 
+                    "'" . $key . "'," . 
+                    $value['votes'] . ")" . ",";  
+            }
+            else {
+                $data_1979_string .= 'null' . "," . "'" . $value['candidate'] . "'" . ",";
+            }
+        }
+    }
+
+    $data_1979_string .= "],";
+
+    $data = $data_1979_string .
+            "['1983', 
+                12824, 'some text or other', 
+                16485 , 'some text',
+                12830, 'actually the SDP',
+                null, 'some text',
+                450, 'actually the Ecology party',
+                2256 , 'some text',
+                null, 'some text',],
+            ['1987', 
+                18211, 'some text or other', 
+                16352 , 'some text',
+                10900, 'actually the SDP',
+                null, 'some text',
+                440, 'some text',
+                2455, 'some text',
+                null, 'some text',],
+            ['1992', 
+                18485, createTooltip('Nigel Griffiths', 'Labour', 18485), 
+                14309, 'some text',
+                5961, 'some text',
+                null, 'some text',
+                null, 'some text',
+                5727, 'some text',
+                null, 'some text',],
+            ['1997', 
+                20993, 'Nigel Griffiths (Labour): 20,993 votes, or 46.8% of the turnout', 
+                9541, 'some text',
+                7911, 'some text',
+                null, 'some text',
+                null, 'some text',
+                5791, 'some text',
+                null, 'some text',],
+            ['2001', 
+                15671, 'Nigel Griffiths (Labour): 15,671 votes, or 42.2% of the turnout', 
+                6172, 'some text',
+                10172, 'some text',
+                null, 'some text',
+                null, 'some text',
+                3683, 'some text',
+                null, 'some text',],
+            ['2005', 
+                14188, 'Nigel Griffiths (Labour): 14,188 votes, or 33.2% of the turnout', 
+                10291, 'some text',
+                13783, 'some text',
+                null, 'some text',
+                1387, 'some text',
+                414, 'some text',
+                null, 'some text',],
+            ['2010', 
+                15215, 'Ian Murray (Labour): 14,188 votes, or 34.7% of the turnout', 
+                9452, 'some text',
+                14899, 'some text',
+                null, 'some text',
+                881, 'some text',
+                3354, 'some text',
+                null, 'some text',]"
 
 ?>
